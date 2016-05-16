dd61b77f4b4d3d063f5ab3c44af832a56f8a090c
Jose Ramirez Imac <Jrramirez1994@gmail.com>
2016-05-16 02:11:12 -0700

    created the base for navigating through the galleries
    Easy way to differentiate sections and loop photos using AngularJS.
    Today we created the plan in which creating the app will be really easy
    and fun.
    
    TODOS:
    - Create looping systems once the user clicks the image where they’ll
    be lead to a large description followed by a close up of the image
    -Set it up so that after the image the user is lead to the next image
    in the sequence.
    - the user should be able to click back on the ‘X’ button to the area
    where the image is located
    - DESIGN DESIGN DESIGN!!!!!!
    - being able to limit the amount of images originally showed to 4 lines
    and have the user click on the arrow button to reveal more.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,45 @@
  * Controller of the joseQApp
  */
 angular.module('joseQApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', [ '$scope' , function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'jquery'
     ];
 
-  $scope.class= 'OnDotCom';
-      
-  });
+  
+$scope.myImages = this;
+
+  $scope.myImages.list = [
+    {
+      Title: 'Squares',
+      Desc: 'their like cool squares',
+      img: 'http://www.ernst-haas.com/colorGallery/abstract/colorAbstract11.jpg'
+    },
+    {
+      Title: 'Squares',
+      Desc: 'their like cool squares',
+      img: 'http://animprobablelife.com/wp-content/uploads/2013/11/Haas.Traffic-Mexico-City-1963_2.jpg'
+    },
+    {
+      Title: 'Squares',
+      Desc: 'their like cool squares',
+      img: 'https://www.wgsn.com/blogs/wp-content/uploads/2012/02/Ernst-Haas-Color-Correction-Christophe-Guy-Zurich-33.jpg'
+    },
+    {
+      Title: 'Squares',
+      Desc: 'their like cool squares',
+      img: 'http://www.ernst-haas.com/colorGallery/abstract/colorAbstract11.jpg'
+    }
+];
+
+  $scope.getImagePath = function(imageName) {
+myImages.list.push({title:myImages.Title});
+};
+
+
+
+
+  }]);
